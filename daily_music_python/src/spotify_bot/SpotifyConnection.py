6a6da4cd14df79808cde553791ad9b63e9839106
targabor <tar.gabor14@gmail.com>
2023-03-29 19:59:07 +0200

    Spotify Token refreshing

--- daily_music_python/src/spotify_bot/SpotifyConnection.py
+++ daily_music_python/src/spotify_bot/SpotifyConnection.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 from typing import TYPE_CHECKING
 from src.python_logger.Logger import Logger
@@ -8,8 +9,14 @@ if TYPE_CHECKING:
 
 
 class SpotifyConnection:
-    # Spotify authentication URL for APIs
+    # Spotify authentication URL for auth
     __auth_url = 'https://accounts.spotify.com/api/token'
+
+    # Basic URL for API calls
+    __base_url = 'https://api.spotify.com/v1/'
+
+    # The time in seconds when the current token expires
+    __token_expires = 0.0
 
     def __init__(self, clientID: str, clientSecret: str):
         """Create a Spotify Web API connection with client ID and client secret.
@@ -17,12 +24,15 @@ class SpotifyConnection:
         Args:
             clientID (str): Client ID of the application
             clientSecret (str): Client Secret of the application
+
+        Raises:
+            Exception : When the connection is unsuccessful.
         """
         self.__clientID = clientID
         self.__clientSecret = clientSecret
 
-    def connect(self):
-        """Connects to the web API with the given ID and secret"""
+    def __refresh_token(self):
+        """Refreshes the access token if it's expired"""
         auth_response = requests.post(SpotifyConnection.__auth_url, {
             'grant_type': 'client_credentials',
             'client_id': self.__clientID,
@@ -32,16 +42,17 @@ class SpotifyConnection:
         auth_response_data = auth_response.json()
 
         # If there is an error while connecting just raise it
-        if auth_response.has_key('error'):
+        if 'error' in auth_response_data.keys():
             raise Exception(auth_response['error_description'])
-        print(auth_response)
-        self.__auth_token = None
+        self.__access_token = auth_response_data['access_token']
+        self.__token_expires = time.time() + auth_response_data['expires_in']
 
 
+# Some test shit
 connector = SpotifyConnection(
     'b2fbeaacf12d4b329a0e67000b92e356', '2e0555828d3f426a884f7bacb440e0d6')
 
 try:
-    connector.connect()
+    connector.get_token()
 except Exception as e:
     Logger.error(e)
