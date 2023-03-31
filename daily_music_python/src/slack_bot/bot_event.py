6a4c2017d545ce82b8b9c3c61b26c300110b9c31
targabor <tar.gabor14@gmail.com>
2023-03-31 15:40:00 +0200

    gunicorn log test

--- daily_music_python/src/slack_bot/bot_event.py
+++ daily_music_python/src/slack_bot/bot_event.py
@@ -12,12 +12,14 @@ app = Flask(__name__)
 slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
 
 client = slack.WebClient(token=SLACK_TOKEN)
-client.chat_postMessage(channel='#bot-test', text='Hello World!')
+
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
 
 
 @ slack_event_adapter.on('message')
 def message(payload):
-    app.logger.info(payload)
+    app.logger.warning(payload)
 
 
 if __name__ == "__main__":
