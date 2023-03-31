bcac5c1ad001d6f64fd8bfffaaa15976ff5d4d4b
targabor <tar.gabor14@gmail.com>
2023-03-31 17:33:53 +0200

    test the message event

--- daily_music_python/src/slack_bot/bot_event.py
+++ daily_music_python/src/slack_bot/bot_event.py
@@ -52,7 +52,7 @@ def message(payload):
     channel_id = event.get('channel')
     user_id = event.get('user')
     text = event.get('text')
-
+    gunicorn_logger.warning(f'{channel_id} \t {user_id} \t {text}')
     if text == "hi":
         client.chat_postMessage(channel=channel_id, text="Hello")
 
