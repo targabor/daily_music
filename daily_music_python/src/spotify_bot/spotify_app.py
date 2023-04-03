e63fcb9135d2d6edfdf255b30da2214642aa8f1c
targabor <tar.gabor14@gmail.com>
2023-04-03 07:47:39 +0200

    Added helper, to avoid helper code being in the Flask app file

--- daily_music_python/src/spotify_bot/spotify_app.py
+++ daily_music_python/src/spotify_bot/spotify_app.py
@@ -1,12 +1,20 @@
 import slack
 import logging
+import os
+
 
 from flask import Flask, make_response, request
+from src.spotify_bot.SpotifyConnection import SpotifyConnection
 
 app = Flask(__name__)
 
 gunicorn_logger = logging.getLogger('gunicorn.error')
 app.logger.handlers = gunicorn_logger.handlers
+
+clientId = os.environ['clientId']
+clientSecret = os.environ['clientSecret']
+
+connection = SpotifyConnection(clientId, clientSecret)
 
 
 @app.route('/status', methods=['GET'])
