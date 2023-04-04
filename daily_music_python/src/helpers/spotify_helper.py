91a25d9645e036606f2acbfc413d5c1eda7d2966
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-04 23:10:16 +0200

    add docstring

--- daily_music_python/src/helpers/spotify_helper.py
+++ daily_music_python/src/helpers/spotify_helper.py
@@ -3,6 +3,9 @@ def clean_track_data(track: dict):
     """
         Returns:
             track in list, duplicated if multiple artists present
+
+        Args:
+            track: response data from spotify api call on spotify_id
     """
     res = []
     track_artist = {"spotify_id": track['id'],
