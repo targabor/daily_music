cc1e7ee5c3d380518f2d383fb6b106f744b97979
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-04 23:08:23 +0200

    add function to format spoty track response

--- daily_music_python/src/helpers/spotify_helper.py
+++ daily_music_python/src/helpers/spotify_helper.py
@@ -1,8 +1,22 @@
 
-
 def clean_track_data(track: dict):
     """
         Returns:
             track in list, duplicated if multiple artists present
     """
-    pass
+    res = []
+    track_artist = {"spotify_id": track['id'],
+                    "artist_id": "N/A",
+                    "title": track['name'],
+                    "popularity": track['popularity']}
+    
+    for artist in track["artists"]:
+        track_artist["artist_id"] = artist["id"]
+        track_artist["artist_name"] = artist["name"]
+        if "genres" in artist:
+            track_artist["genres"] = [genre for genre in arist["genres"]]
+        res.append(track_artist)
+    else:
+        res.append(track_artist)
+
+    return res
