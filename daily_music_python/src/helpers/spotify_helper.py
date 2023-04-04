6919482bf80e36caaf3e18c2174a88164122f9c7
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-04 23:37:39 +0200

    create helper function to match artists to genres

--- daily_music_python/src/helpers/spotify_helper.py
+++ daily_music_python/src/helpers/spotify_helper.py
@@ -17,9 +17,28 @@ def clean_track_data(track: dict):
         track_artist["artist_id"] = artist["id"]
         track_artist["artist_name"] = artist["name"]
         if "genres" in artist:
-            track_artist["genres"] = [genre for genre in arist["genres"]]
+            track_artist["genres"] = [genre for genre in artist["genres"]]
         res.append(track_artist)
     else:
         res.append(track_artist)
 
     return res
+
+
+def make_genre_to_artist_pairs(artist_genres: list):
+    """ Creates pairs for matching table for artists and its genres
+
+        Returns:
+            List of two element list:
+                e.g.: [[atrist_id1, genre_id1], [[atrist_id1, genre_id2]]
+
+        Args:
+            atrist_genres: list containing dict in format [{artist_id: artist_id, genres: [genre1, genre2...]}, ...]
+    """
+    res = []
+    for artist in artist_genres:
+        for genre in artist["genres"]:
+            res.append[artist["artist_id"]]
+            res.append(genre)
+    return res
+                
