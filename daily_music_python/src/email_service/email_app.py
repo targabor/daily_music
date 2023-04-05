6c835c9936f1c1bea0358bd90f793bd3800760c0
targabor <tar.gabor14@gmail.com>
2023-04-05 11:29:15 +0200

    debug

--- daily_music_python/src/email_service/email_app.py
+++ daily_music_python/src/email_service/email_app.py
@@ -2,11 +2,16 @@ import logging
 import smtplib
 import ssl
 import os
-
+import codecs
 
 from flask import Flask, make_response
 from src.snowflake_functions import snowflake_functions
 from src.helpers import email_helper
+from datetime import datetime
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 
 app = Flask(__name__)
 
@@ -30,15 +35,31 @@ def status():
 def send_mails_out():
     """It sends the weekly newsletter for those who subscribed to it"""
     html_path = os.environ['HTML_PATH']
-    base_msg = email_helper.generate_template_message(
-        from_email, image_path, html_path)
+    gunicorn_logger.info(html_path)
+    body = ''
+    gunicorn_logger.info(html_path)
+    with codecs.open(html_path, 'r') as f:
+        body = f.read()
+
+    gunicorn_logger.info(body)
+    body.replace('#current_date', datetime.now().strftime('%Y-%m-%d'))
+    msg = MIMEMultipart()
+    msg['From'] = from_email
+    msg['Subject'] = str(datetime.now().isocalendar().week) + '. Lit Letter'
+    with open(image_path, 'rb') as f:
+        img_data = f.read()
+        image = MIMEImage(img_data)
+        image.add_header('Content-ID', '<image1>')
+        msg.attach(image)
+
+    msg.attach(MIMEText(body, 'html'))
     mail_list = snowflake_functions.get_mail_list()
     gunicorn_logger.info(mail_list)
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
         server.login(from_email, app_password)
         for mail in mail_list:
-            server.sendmail(from_email, mail, base_msg.as_string())
+            server.sendmail(from_email, mail, msg.as_string())
             gunicorn_logger.info(f'Email sent to {mail}')
 
     return make_response('Emails are sent', 200)
--- daily_music_python/src/helpers/email_helper.py
+++ daily_music_python/src/helpers/email_helper.py
@@ -17,22 +17,5 @@ def generate_template_message(from_email: str, image_path: str, html_path: str, 
     Returns:
         _type_: _description_
     """
-    body = ''
-    gunicorn_logger.info(html_path)
-    with codecs.open(html_path, 'r') as f:
-        body = f.read()
 
-    gunicorn_logger.info(body)
-    body.replace('#current_date', datetime.now().strftime('%Y-%m-%d'))
-    msg = MIMEMultipart()
-    msg['From'] = from_email
-    msg['Subject'] = str(datetime.now().isocalendar().week) + '. Lit Letter'
-    with open(image_path, 'rb') as f:
-        img_data = f.read()
-        image = MIMEImage(img_data)
-        image.add_header('Content-ID', '<image1>')
-        msg.attach(image)
-
-    msg.attach(MIMEText(body, 'html'))
-
-    return msg
+    pass
