9f014a933571d79393cfe0454f38debf36aaf710
Alba Nemeth-Buhin <buhina@starschema.com>
2023-04-11 17:52:28 +0200

    add file to trigger endpoint on local windows

--- daily_extract.bat
+++ daily_extract.bat
@@ -1,8 +1,24 @@
 @echo off
 
-echo "starting daily extraction"
-curl http://localhost:5001/extract_data
-curl http://localhost:5000/get_id_for_youtube_songs
+echo ---------------------------------------------
+echo starting daily extraction
+
+echo -
+echo getting tracks from slack api
+REM curl http://localhost:5001/extract_data
+
+echo -
+echo matching up yputube links to spotify tracks
+REM curl http://localhost:5000/get_id_for_youtube_songs
+
+echo -
+echo getting data for spotify tracks
 curl http://localhost:5000/get_data_for_tracks
+
+echo -
+echo getting artists genre information
 curl http://localhost:5000/get_data_for_artists
-echo "daily extraction done"
+
+echo -
+echo ---------------------------------------------
+echo daily extraction done
